c6009eb3968da800c601c0d6e87d0ab6c496d0df
Hussain Abbas <hussain@hussainabbas.com>
2016-02-27 17:54:30 -0500

    auto git commit, push, and pull on server

--- pull4me.sh
+++ pull4me.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+read -p "Commit description: " desc
+git checkout master
+git add -A && \
+git commit -am "$desc" && \
+git push
 ssh <user>@<host> 'bash -s' < ./pull4me-scripts.sh
 # replace <user> and <host> with real stuff
